09409e87a3a058a5fede91b863d4eec143f50c9b
Bojan B <83359157+bonjadev@users.noreply.github.com>
2023-04-29 03:30:33 +0200

    First commit

---
+++ Reverse_number_c#.cs
@@ -0,0 +1,20 @@
+﻿namespace c__Reverse_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide input number: ");
+            int input = int.Parse(Console.ReadLine()); //konverzija u integer i citanje te vrednosti u promenjivoj int
+
+            int remainder, reverse = 0;
+            while (input > 0) //ispitivanje uslova 
+            {
+                remainder = input % 10;
+                reverse = reverse * 10 + remainder;
+                input = input / 10;
+            }
+            Console.WriteLine($"reverse number is {reverse}"); //ispisivanje vrednost na mesto placeholdera koji je oznacen sa {}
+        }
+    }
+}
